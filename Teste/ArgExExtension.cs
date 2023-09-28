9bfb9aed4b99a20596d9a2d23aaa498fb41f129a
icrcode <icarobotelhosocial@gmail.com>
2023-09-27 21:04:59 -0300

    programação moderna é maravilhosa

---
+++ Teste/ArgExExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal static class ArgExExtension
+    {
+        public static void ComMensagem(
+            //veja o poder do ponteiro
+            this ArgumentException excecao,
+            string mensagem
+            ) 
+        {
+        
+            if ( excecao.Message == mensagem ) 
+            { 
+                Assert.True( true );
+            }
+            else
+            {
+                Assert.False( true );
+            }
+        
+        }
+    }
+}
